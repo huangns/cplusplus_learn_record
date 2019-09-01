5bb40fb6320eaaf20351636aef2d43194807b06a
huangns <34503774+huangns@users.noreply.github.com>
2019-09-01 18:15:15 +0800

    add some member function introduction of shared_ptr

--- src/smart_pointer/shared_ptr_test.cpp
+++ src/smart_pointer/shared_ptr_test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <memory>
-
+#include <string>
+#include <vector>
 void share_ptr_test_func(std::shared_ptr<int> spobj)
 {
   std::cout<<"spobj value: "<<*spobj<<std::endl;
@@ -11,6 +12,20 @@ std::shared_ptr<int> share_ptr_return_func(const std::shared_ptr<int>& spobj)
 {
   return spobj;
 }
+
+void myDelete(int* p)
+{
+  std::cout<<"launch my delete function."<<std::endl;
+  delete p;
+}
+
+template <typename T>
+std::shared_ptr<T> make_shared_ptr_array(std::size_t size)
+{
+  return std::shared_ptr<T>(new T[size],std::default_delete<T[]>());
+}
+
+
 int main()
 {
   std::cout<<"share_ptr learn."<<std::endl;
@@ -87,8 +102,99 @@ int main()
   //若pi不是唯一指向该对象的指针，则不释放pi指向的对象，但指向该对象的引用计数会减少1
   //同时让pi指向新对象。
   
+  //2.4 *,解引用
+  //2.5get() 考虑到有些函数的参数需要的是一个内置裸指针，而不是智能指针
+  //p.get():返回p中保存的指针（裸指针），小心使用，如果智能指针释放了所指向的对象，那么这个返回的裸指针也就无效了
+  std::shared_ptr<int> getp(new int(678));
+  std::cout<<"getp value: "<<*getp<<std::endl;
+  int *p=getp.get();
+  *p = 786;
+  std::cout<<"getp value after: "<<*getp<<std::endl;
   
+  //swap
+  std::shared_ptr<std::string> ps1(new std::string("ps1"));
+  std::shared_ptr<std::string> ps2(new std::string("ps2"));
+  std::cout<<"ps1 value: "<<*ps1<<", ps2 value: "<<*ps2<<std::endl;
+  std::swap(ps1,ps2);
+  std::cout<<"after swap1, ps1 value: "<<*ps1<<", ps2 value: "<<*ps2<<std::endl;
+  ps1.swap(ps2);
+  std::cout<<"after swap2, ps1 value: "<<*ps1<<", ps2 value: "<<*ps2<<std::endl;
+  //2.7 =nullptr
+  //a. 将所指向的对象 引用计数减去1， 若引用计数变为0， 则释放智能指针所指向的对象
+  //b. 将智能指针置空
+  std::shared_ptr<int> nptr(new int(99));
+  nptr=nullptr;
+  //2.8智能指针作为一个判断条件
+  nptr.reset(new int(34));
+  if(nullptr!=nptr)
+  {
+    std::cout<<"nptr pointer one obj."<<std::endl;
+  }else
+  {
+    std::cout<<"nptr don't pointer any obj."<<std::endl;
+  }
+  nptr.reset();
+  if(nullptr!=nptr)
+  {
+    std::cout<<"nptr pointer one obj."<<std::endl;
+  }else
+  {
+    std::cout<<"nptr don't pointer any obj."<<std::endl;
+  }
   
+  //2.9 自定义删除qi
+  std::shared_ptr<int> mdelete_ptr(new int(76),myDelete);
+  mdelete_ptr.reset();
+  //也可以用lambda函数
+  std::shared_ptr<int> mlambda_delete_ptr(new int(45),[](int* p){std::cout<<"launch lambda delete func."<<std::endl;delete p;});
   
+  //有些情况，默认删除qi解决不了（用shared_ptr管理动态数组），需要我们提供自己制定的删除qi,
+  std::shared_ptr<int> sarray_p(new int[10],[](int*p){delete []p;});
+  
+  //可用default_delete来做删除qi,default_delete是标准库里的模板类
+  //std::shared_ptr<A> pA(new A[10],std::default_delete<A[]>());
+  std::shared_ptr<int> pint(new int[10],std::default_delete<int[]>());
+  for(int i=0;i<10;++i)
+  {
+    pint.get()[i]=i*i;
+  }
+  for(int i=0;i<10;++i)
+  {
+    std::cout<<"pint["<<i<<"]: "<<pint.get()[i]<<std::endl;
+  }
+  
+  std::shared_ptr<int> pint2(make_shared_ptr_array<int>(10));
+  for(int i=0;i<10;++i)
+  {
+    pint2.get()[i]=(i+1)*(i+1);
+  }
+  for(int i=0;i<10;++i)
+  {
+    std::cout<<"pint2["<<i<<"]: "<<pint2.get()[i]<<std::endl;
+  }
+  
+  //pint.get()[0]=1;
+  //std::shared_ptr<A[]> pA(new A[10]);
+  //????
+  //b 制定删除其额外说明
+  //就算是两个shared_ptr制定了不同的删除其，只要他们所指向的对象类型相同，那么这两个shared_ptr也属于同一个类型
+  auto lambda1=[](int* p)
+  {
+    std::cout<<"lambda 1 delete."<<std::endl;delete p;
+  };
+  auto lambda2=[](int* p)
+  {
+    std::cout<<"lambda 2 delete."<<std::endl;delete p;
+  };
+  
+  std::shared_ptr<int> psl1(new int(100),lambda1);
+  std::shared_ptr<int> psl2(new int(200),lambda2);
+  
+  psl2=psl1;
+  std::cout<<"-------------------------"<<std::endl;
+  
+  //类型相同，就代表可以放到元素类型为该对象类型的容器里来
+  std::vector<std::shared_ptr<int>> pvec{psl1,psl2};
+  //make_shared是提倡的生成shared_ptr的方法，但是make_shared这种方法，让我们没办法制定自己的删除其
   return 0;
 }
